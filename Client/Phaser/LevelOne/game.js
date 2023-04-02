e2d05963feec5bb8502df23d9919a46020a12760
Andyh14 <124717559+Andyh14@users.noreply.github.com>
2023-04-02 17:54:09 -0400

    Update game.js
    Added a listen for buttons pressed from menu html file in the submenu folder. There are three listens: One for the menu to stop displaying the menu when a button pressed. One for the play button that will start the game. One for the load button which will send you to another page.

--- Client/Phaser/LevelOne/game.js
+++ Client/Phaser/LevelOne/game.js
@@ -1,19 +1,69 @@
-var config = {
-    type: Phaser.AUTO,
-    mode: Phaser.Scale.Fit,
-    width: 1280,
-    height: 720,
-    physics: {
+// Load the menu screen before starting the Phaser game
+window.onload = function() {
+    var menuScreen = document.getElementById("menu-screen");
+    var playButton = document.getElementById("play-button");
+    var loadButton = document.getElementById("load-button");
+  
+    playButton.onclick = function() {
+      menuScreen.style.display = "none";
+      startGame();
+    }
+    loadButton.onclick = function() {
+        menuScreen.style.display = "none";
+        /*Whatever load does*/ 
+        //Maybe this if we want to make a whole new page. If not we can make a drop down menu
+        //The href is just a random name made. Have not implemented the dropdown yet.
+        window.location.href = "input.html";
+    
+        // startGame();
+    }
+  }
+  
+
+  /*This should set the configuration for the Phaser game instance and create the game object.
+  When the user clicks the play button on the menu screen, the startGame() function is called, 
+  which creates the Phaser game object using the configuration defined in the main JavaScript file.
+  */
+  function startGame() {
+    var config = {
+      type: Phaser.AUTO,
+      mode: Phaser.Scale.Fit,
+      width: 1280,
+      height: 720,
+      physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 300 },
-            debug: false
+          gravity: { y: 300 },
+          debug: false
         }
-    },
-    scale: {
+      },
+      scale: {
+        parent: 'myGame',
         autoCenter: Phaser.Scale.CENTER_BOTH
-    },
-    scene: [LevelOne]
-};
+      },
+      scene: [LevelOne]
+    };
+  
+    var game = new Phaser.Game(config);
+  }
 
-var game = new Phaser.Game(config);
+//Original Code 
+// var config = {
+//     type: Phaser.AUTO,
+//     mode: Phaser.Scale.Fit,
+//     width: 1280,
+//     height: 720,
+//     physics: {
+//         default: 'arcade',
+//         arcade: {
+//             gravity: { y: 300 },
+//             debug: false
+//         }
+//     },
+//     scale: {
+//         autoCenter: Phaser.Scale.CENTER_BOTH
+//     },
+//     scene: [LevelOne]
+// };
+
+// var game = new Phaser.Game(config);
