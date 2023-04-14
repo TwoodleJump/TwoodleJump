abc791b4518474a4ceabfb0f5c61d62e45c7ac76
Dylan Wilmoth <dylanw2@ad.umbc.edu>
2023-04-14 17:35:45 -0400

    Added clientside backend to sent html page when server is loaded. Add this js file to index.js in /Server.

--- Server/index.js
+++ Server/index.js
@@ -4,12 +4,14 @@
 const dotenv = require("dotenv").config();
 const mongoose = require("mongoose");
 const SavedGames = require("./routes/SavedGamesRoutes");
+const ClientSide = require("./routes/ClientSide")
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(SavedGames);
+app.use(ClientSide);
 
 /*
     Listening on port 3001 or 5000
---
+++ Server/routes/ClientSide.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, '/../../Client/Phaser')))
+app.use(express.static(path.join(__dirname, '/../../Client/Phaser/LevelOne')))
+
+
+app.get("/", async (req, res) => {
+    try {
+      res.sendFile(path.resolve(__dirname + '/../../Client/Phaser/LevelOne/index.html'));
+    } catch (error) {
+      res.status(404).send({message: error.message})
+    }
+  })
+  
+module.exports = app;
