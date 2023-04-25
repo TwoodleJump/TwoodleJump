98fd57aaac0d55b0048c050816b36c9dbcd2be1e
Dylan Wilmoth <dylanwilmoth@Dylans-MacBook-Air-2.local>
2023-04-25 13:41:52 -0400

    Sorts and displays top 5(or less) users in the game. Added css to make table easier to read

--- Client/Submenus/Leaderboard/index.js
+++ Client/Submenus/Leaderboard/index.js
@@ -1,3 +1,40 @@
+// create a table element
+const table = document.createElement("table");
+
+// create table header
+const headerRow = table.insertRow();
+const nameHeader = headerRow.insertCell();
+nameHeader.textContent = "Player Name";
+const winsHeader = headerRow.insertCell();
+winsHeader.textContent = "Wins";
+
+const options = {
+    method: 'GET'
+}
+
+// fetch the data from the "players" route
+fetch("/players", options)
+  .then(response => response.json())
+  .then(data => {
+    // sort the data by number of wins in descending order
+    data.sort((a, b) => b.numWins - a.numWins);
+    
+    // loop through the data and create table rows
+    for (let i = 0; i < Math.min(data.length, 5); i++) {
+      const row = table.insertRow();
+      const nameCell = row.insertCell();
+      nameCell.textContent = data[i].player;
+      const winsCell = row.insertCell();
+      winsCell.textContent = data[i].numWins;
+    }
+    
+    // add the table to the HTML document
+    document.body.appendChild(table);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
 document.getElementById("Results").onclick = function () {
     location.href = "/WinnerScreen";
 };
