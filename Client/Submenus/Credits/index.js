427fab666bc62e75062fff7fa17c3d30a41e9cd4
andyh1 <andyh1@umbc.edu>
2023-05-02 03:40:50 -0400

    Added music for each level being played in teh background

--- Client/Submenus/Credits/index.js
+++ Client/Submenus/Credits/index.js
@@ -1,8 +1,17 @@
 document.getElementById("MainMenu").onclick = function () {   
     location.href = "/";
 };
-
+const audio1 = document.getElementById("background-audio-1");
+const audio2 = document.getElementById("background-audio-2");
+const audio3 = document.getElementById("background-audio-3");
 const video = document.getElementById("background-video");
+
+const audios = [
+  "/Client/Submenus/Images/Bob-ombBattlefield.mp3",
+  "/Client/Submenus/Images/coolMountain.mp3",
+  "/Client/Submenus/Images/mansion.mp3",
+
+];
 const videos = [
   "/Client/Submenus/images/LevelOneGameplay.mp4",
   "/Client/Submenus/images/LevelTwoGameplay.mp4",
@@ -11,10 +20,16 @@ const videos = [
 const randomIndex = Math.floor(Math.random() * videos.length);
 
 video.src = videos[randomIndex];
+document.getElementById("background-audio-" + (randomIndex + 1)).play();
+
 
 video.addEventListener("ended", () => {
   const randomIndex = Math.floor(Math.random() * videos.length);
   video.src = videos[randomIndex];
-  video.currentTime = 0;
-  video.play();
+  // video.currentTime = 0;
+  // video.play();
+  audios[randomIndex].currentTime = 0;
+  audios[randomIndex].play();
 });
+
+
