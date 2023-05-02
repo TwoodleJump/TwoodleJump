a4a09844c82d585efe0bed5f4a621998e6e138a4
andyh1 <andyh1@umbc.edu>
2023-05-02 03:12:09 -0400

    Added changes to credit screen and how to play to match menu screen more, and update pictures in the how to play

--- Client/Submenus/Credits/index.js
+++ Client/Submenus/Credits/index.js
@@ -1,3 +1,20 @@
 document.getElementById("MainMenu").onclick = function () {   
     location.href = "/";
 };
+
+const video = document.getElementById("background-video");
+const videos = [
+  "/Client/Submenus/images/LevelOneGameplay.mp4",
+  "/Client/Submenus/images/LevelTwoGameplay.mp4",
+  "/Client/Submenus/images/LevelThreeGameplay.mp4",
+];
+const randomIndex = Math.floor(Math.random() * videos.length);
+
+video.src = videos[randomIndex];
+
+video.addEventListener("ended", () => {
+  const randomIndex = Math.floor(Math.random() * videos.length);
+  video.src = videos[randomIndex];
+  video.currentTime = 0;
+  video.play();
+});
